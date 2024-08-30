b0ab0a924e32b9f6f7c9ea4b3ca5cb2b40cf0f78
romansechin <romansechin@gmail.com>
2024-08-30 16:19:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    class Guy
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString() => $"a {Age}-year-old named {Name}";
+    }
+    
+    static void ModifyAnIntAndGuy(ref int valueRef, ref Guy guyRef)
+    {
+        valueRef += 10;
+        guyRef.Name = "Bob";
+        guyRef.Age = 37;
+    }
+    static void Main(string[] args)
+    {
+        var i = 1;
+        var guy = new Guy() { Name = "Joe", Age = 26 };
+        Console.WriteLine("i is {0} and guy is {1}", i, guy);
+        ModifyAnIntAndGuy(ref i, ref guy);
+        Console.WriteLine("Now i is {0} and guy is {1}", i, guy);
+    }       
+}
